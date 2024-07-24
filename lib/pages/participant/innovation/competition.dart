0cc35c6ef37cc4f561d439a93ebdb461682c37bf
ayeshgallege <ayeshpramuditha2000@gmail.com>
2024-07-25 03:00:55 +0530

    add i'm a reaseracher competition page

--- lib/pages/participant/innovation/competition.dart
+++ lib/pages/participant/innovation/competition.dart
@@ -1,24 +1,125 @@
-// // ignore_for_file: prefer_const_constructors
+// // // ignore_for_file: prefer_const_constructors
+
+// // import 'package:flutter/material.dart';
+// // import 'package:youtube_1/pages/participant/innovation/home_page.dart'; // Import HomePage
+// // import 'package:youtube_1/pages/participant/innovation/profile_view.dart'; // Import ProfilePage
+
+// // class CompetitionPage extends StatelessWidget {
+// //   const CompetitionPage({Key? key}) : super(key: key);
+
+// //   void _navigateToHomePage(BuildContext context) {
+// //     Navigator.push(
+// //       context,
+// //       MaterialPageRoute(builder: (context) => HomePage()),
+// //     );
+// //   }
+
+// //   void _navigateToProfilePage(BuildContext context) {
+// //     Navigator.push(
+// //       context,
+// //       MaterialPageRoute(builder: (context) => ProfileViewPage()),
+// //     );
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text('Competition'),
+// //         backgroundColor: Colors.blue[800],
+// //       ),
+// //       body: Center(
+// //         child: Column(
+// //           mainAxisAlignment: MainAxisAlignment.center,
+// //           children: <Widget>[
+// //             Icon(
+// //               Icons.search,
+// //               size: 150,
+// //               color: Colors.blue[800],
+// //             ),
+// //             SizedBox(height: 20),
+// //             Text(
+// //               'Search Competition',
+// //               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+// //             ),
+// //             SizedBox(height: 10),
+// //             Text(
+// //               'Add your competition details here!',
+// //               style: TextStyle(fontSize: 16),
+// //             ),
+// //           ],
+// //         ),
+// //       ),
+// //       bottomNavigationBar: BottomNavigationBar(
+// //         items: [
+// //           BottomNavigationBarItem(
+// //             icon: Icon(Icons.home),
+// //             label: 'Home',
+// //           ),
+// //           BottomNavigationBarItem(
+// //             icon: Icon(Icons.shopping_bag),
+// //             label: 'Packages',
+// //           ),
+// //           BottomNavigationBarItem(
+// //             icon: Icon(Icons.account_circle),
+// //             label: 'Profile',
+// //           ),
+// //         ],
+// //         selectedItemColor: Colors.blue[800],
+// //         onTap: (index) {
+// //           switch (index) {
+// //             case 0:
+// //               _navigateToHomePage(context); // Navigate to home page
+// //               break;
+// //             case 1:
+// //               // Handle action for packages or navigate to packages page
+// //               break;
+// //             case 2:
+// //               _navigateToProfilePage(context); // Navigate to profile page
+// //               break;
+// //           }
+// //         },
+// //       ),
+// //     );
+// //   }
+// // }
+// //import 'package:flutter/material.dart';
+// //import 'package:youtube_1/pages/participant/innovation/home_page.dart';
+// //import 'package:youtube_1/pages/participant/innovation/profile_view.dart';
 
 // import 'package:flutter/material.dart';
-// import 'package:youtube_1/pages/participant/innovation/home_page.dart'; // Import HomePage
-// import 'package:youtube_1/pages/participant/innovation/profile_view.dart'; // Import ProfilePage
+// import 'package:youtube_1/pages/participant/innovation/home_page.dart';
+// import 'package:youtube_1/pages/participant/innovation/profile_view.dart';
+// import 'package:youtube_1/pages/participant/innovation/custom_bottom_navigation_bar.dart'; // Import the new widget
 
-// class CompetitionPage extends StatelessWidget {
+// class CompetitionPage extends StatefulWidget {
 //   const CompetitionPage({Key? key}) : super(key: key);
 
-//   void _navigateToHomePage(BuildContext context) {
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(builder: (context) => HomePage()),
-//     );
-//   }
+//   @override
+//   _CompetitionPageState createState() => _CompetitionPageState();
+// }
 
-//   void _navigateToProfilePage(BuildContext context) {
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(builder: (context) => ProfileViewPage()),
-//     );
+// class _CompetitionPageState extends State<CompetitionPage> {
+//   int _currentIndex = 0;
+
+//   void _onBottomNavBarTapped(int index) {
+//     switch (index) {
+//       case 0:
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => HomePage()),
+//         );
+//         break;
+//       case 1:
+//         // Handle action for packages or navigate to packages page
+//         break;
+//       case 2:
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => ProfileViewPage()),
+//         );
+//         break;
+//     }
 //   }
 
 //   @override
@@ -50,47 +151,20 @@
 //           ],
 //         ),
 //       ),
-//       bottomNavigationBar: BottomNavigationBar(
-//         items: [
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.shopping_bag),
-//             label: 'Packages',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.account_circle),
-//             label: 'Profile',
-//           ),
-//         ],
-//         selectedItemColor: Colors.blue[800],
-//         onTap: (index) {
-//           switch (index) {
-//             case 0:
-//               _navigateToHomePage(context); // Navigate to home page
-//               break;
-//             case 1:
-//               // Handle action for packages or navigate to packages page
-//               break;
-//             case 2:
-//               _navigateToProfilePage(context); // Navigate to profile page
-//               break;
-//           }
-//         },
+//       bottomNavigationBar: CustomBottomNavigationBar(
+//         currentIndex: _currentIndex,
+//         onTap: _onBottomNavBarTapped,
 //       ),
 //     );
 //   }
 // }
-//import 'package:flutter/material.dart';
-//import 'package:youtube_1/pages/participant/innovation/home_page.dart';
-//import 'package:youtube_1/pages/participant/innovation/profile_view.dart';
+
 
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart'; // Import the WebView package
 import 'package:youtube_1/pages/participant/innovation/home_page.dart';
 import 'package:youtube_1/pages/participant/innovation/profile_view.dart';
-import 'package:youtube_1/pages/participant/innovation/custom_bottom_navigation_bar.dart'; // Import the new widget
+import 'package:youtube_1/pages/participant/innovation/custom_bottom_navigation_bar.dart'; // Import CustomBottomNavigationBar
 
 class CompetitionPage extends StatefulWidget {
   const CompetitionPage({Key? key}) : super(key: key);
@@ -123,33 +197,23 @@ class _CompetitionPageState extends State<CompetitionPage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    if (WebView.platform == SurfaceAndroidWebView()) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Competition'),
         backgroundColor: Colors.blue[800],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.search,
-              size: 150,
-              color: Colors.blue[800],
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Search Competition',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Add your competition details here!',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
-        ),
+      body: WebView(
+        initialUrl: 'https://icter.lk/i-am-a-researcher/',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: _currentIndex,
