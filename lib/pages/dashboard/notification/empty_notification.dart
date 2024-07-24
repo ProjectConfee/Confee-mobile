d29d40e809eec1bfd63fd1f5eb14d0cd759e1c4d
KeshaliDhananjana <keshdhananjana@gmail.com>
2024-07-24 18:11:58 +0530

    notification pages

---
+++ lib/pages/dashboard/notification/empty_notification.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class EmptyNotification extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(''), // Replace with your background image path
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 130,
+              color: Color(0xFF050C9B),
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10.0, top: 20.0),
+              child: Stack(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            'Notifications',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 4),
+                      Padding(
+                        padding: EdgeInsets.only(left: 34.0),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Stay Updated',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/img/empty_notification.png', // Replace with your image path
+                  width: 300, // Adjust the width and height as needed
+                  height: 300,
+                ),
+                SizedBox(height: 20), // Space between image and text
+                Text(
+                  'No notifications!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10), // Space between image and text
+                Text(
+                  "You don't have any notification at this time.",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      // bottomNavigationBar: HomeBottomBar(),
+    );
+  }
+}
+
